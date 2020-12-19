ad652c30fba73d69ac77b75eb6ac7b3d7fb0ef2a
Guilherme Bagnato <gui.bagnato@gmail.com>
2020-12-19 19:04:13 -0300

    (+) Initial commit

---
+++ fundamentos/01/first.go
@@ -0,0 +1,15 @@
+// Programas executáveis iniciam com pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// Entrypoint
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+
+}
